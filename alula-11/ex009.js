6b401516e5c6cede46e97c9e031e543835c0d40a
lucas daniel <146143426+lucassdani@users.noreply.github.com>
2024-01-12 08:05:37 -0300

    n/a

--- alula-11/ex009.js
+++ alula-11/ex009.js
@@ -1,7 +1,18 @@
-var pais = 'eua'
+function verificar() {
+  var pais = document.querySelector("input#nome");
+  var res = document.querySelector("div#res");
+    res.innerHTML = `<p>você esta vivendo em(no) ${pais}</p>`
+  if (pais === "brasil") {
+    res.innerHTML += "<p>você é brasileiro</p>";
+  } else {
+    res.innerHTML += "<p>você é estrangeiro</p>";
+  }
+}
+
+/*var pais = 'eua'
 console.log(`vivendo em ${pais}`)
 if (pais === 'brasil') {
     console.log('brasileiro')
 }else {
     console.log('estrangeiro')
-}
+}*/
