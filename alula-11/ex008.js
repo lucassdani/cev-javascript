ddda79a0a7109ed59d03b4b667fd2e162a36c4d1
lucas daniel <146143426+lucassdani@users.noreply.github.com>
2024-01-12 07:29:07 -0300

    n/a

---
+++ alula-11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`a velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log('voce ultrapassou o limite de velocidade, MULTADO')
+}
+console.log('dirija sempre usando cinto de segurança')
---
+++ alula-11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'eua'
+console.log(`vivendo em ${pais}`)
+if (pais === 'brasil') {
+    console.log('brasileiro')
+}else {
+    console.log('estrangeiro')
+}
